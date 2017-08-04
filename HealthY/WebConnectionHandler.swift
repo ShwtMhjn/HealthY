a90f2f73ec1fcf16804b6d0a30beb218325cd77f
Mudit Agarwal <mudit.agar@gmail.com>
2017-08-04 19:41:35 +0530

    added network connectivity

--- HealthY/WebConnectionHandler.swift
+++ HealthY/WebConnectionHandler.swift
@@ -2,16 +2,31 @@ import Foundation
 
 class WebConnectionHandler: NSObject, URLSessionDelegate, URLSessionTaskDelegate, URLSessionDataDelegate {
     static var STATUS_CODE_LOGIN_COOKIE = 302
-    var session: URLSession
-    var responseData: NSMutableData
-    var response: URLResponse
-    var dataTask: URLSessionDataTask
+//    var session: URLSession
+//    var responseData: NSMutableData
+//    var response: URLResponse
+//    var dataTask: URLSessionDataTask
 
     //MARK: -- Execute Request --
-
-    func executeRequest(request: URLRequest) {
-
+    override init() {
+        
+        super.init()
+    }
+    func executeRequest() {
+        
+        let urlString = URL(string: "https://raw.githubusercontent.com/psaini0212/healthy-data/master/data/response.json")
+        if let url = urlString {
+            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error)
+                } else {
+                    if let usableData = data {
+                        let json = try? JSONSerialization.jsonObject(with: usableData, options: []);
+                        print(json);
+                    }
+                }
+            }
+            task.resume()
+        }
     }
 }
-
-
